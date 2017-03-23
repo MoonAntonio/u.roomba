0ba727b193db858653ddd9e2f67078f29f12ff4c
Moon Antonio <pinchodev91@gmail.com>
2017-03-23 03:27:45 +0100

    Implementado singleton

--- Roomba Project/Assets/Proyecto Roomba/Scripts/MAlexandria.cs
+++ Roomba Project/Assets/Proyecto Roomba/Scripts/MAlexandria.cs
@@ -1,10 +1,10 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// MAlexandria.cs (00/00/0000)													\\
+// MAlexandria.cs (20/03/2017)													\\
 // Autor: Antonio Mateo (Moon Antonio) 									        \\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// Descripcion:		Manager del sistema											\\
+// Fecha Mod:		20/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
 
 #region Librerias
@@ -13,8 +13,36 @@ using UnityEngine;
 
 namespace MoonPincho
 {
+	/// <summary>
+	/// <para>Manager del sistema</para>
+	/// </summary>
 	public class MAlexandria : MonoBehaviour 
 	{
+		#region Variables Estaticas
+		/// <summary>
+		/// <para>Instancia del manager.</para>
+		/// </summary>
+		public static MAlexandria instance = null;									// Instancia del manager
+		#endregion
 
+		#region Iniciadores
+		/// <summary>
+		/// <para>Carga de MAlexandria.</para>
+		/// </summary>
+		private void Awake()// Carga de MAlexandria
+		{
+			// Singleton
+			if (instance == null)
+			{
+				instance = this;
+			}
+			else if (instance != this)
+			{
+				Destroy(this.gameObject);
+			}
+
+			DontDestroyOnLoad(this.gameObject);
+		}
+		#endregion
 	}
 }
