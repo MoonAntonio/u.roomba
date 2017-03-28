320fead6d1ac7fc7a2954e5dcc2fa9a1329a5f3f
Moon Antonio <pinchodev91@gmail.com>
2017-03-29 01:21:06 +0200

    Ajustado los rayos para comprobar la zona

--- Roomba Project/Assets/Proyecto Roomba/Scripts/Sistemas/Generador.cs
+++ Roomba Project/Assets/Proyecto Roomba/Scripts/Sistemas/Generador.cs
@@ -19,13 +19,57 @@ namespace MoonAntonio.Roomba
 	/// </summary>
 	public class Generador : MonoBehaviour 
 	{
-		public List<GameObject> objetosSpawn = new List<GameObject>();
+
+		public Transform[] perimetro;
+			
+
+		private List<GameObject> objetosSpawn = new List<GameObject>();
+
+
 
 		private void Start()
 		{
 			objetosSpawn.Add((Resources.Load("Objeto01")) as GameObject);
 			objetosSpawn.Add((Resources.Load("Objeto02")) as GameObject);
 			objetosSpawn.Add((Resources.Load("Objeto03")) as GameObject);
+		}
+
+		private void Update()
+		{
+			Ray();
+		}
+
+		private void Ray()
+		{
+			// Linea #1
+			Debug.DrawLine(perimetro[0].position, perimetro[1].position, Color.red);
+			Debug.DrawLine(perimetro[1].position, perimetro[2].position, Color.red);
+			Debug.DrawLine(perimetro[2].position, perimetro[3].position, Color.red);
+			Debug.DrawLine(perimetro[3].position, perimetro[0].position, Color.red);
+
+			// Linea #2
+			Debug.DrawLine(perimetro[0].position, perimetro[0].up * 10, Color.red);
+			Debug.DrawLine(perimetro[1].position, perimetro[1].up * 10, Color.red);
+			Debug.DrawLine(perimetro[2].position, perimetro[2].up * 10, Color.red);
+			Debug.DrawLine(perimetro[3].position, perimetro[3].up * 10, Color.red);
+
+			// Linea #3
+			Debug.DrawLine(perimetro[0].position + new Vector3(0, 1, 0), perimetro[1].position, Color.magenta);
+			Debug.DrawLine(perimetro[1].position + new Vector3(0, 1, 0), perimetro[2].position, Color.magenta);
+			Debug.DrawLine(perimetro[2].position + new Vector3(0, 1, 0), perimetro[3].position, Color.magenta);
+			Debug.DrawLine(perimetro[3].position + new Vector3(0, 1, 0), perimetro[0].position, Color.magenta);
+
+			// Linea #4
+			Debug.DrawLine(perimetro[0].position, perimetro[1].position + new Vector3(0, 1, 0), Color.magenta);
+			Debug.DrawLine(perimetro[1].position, perimetro[2].position + new Vector3(0, 1, 0), Color.magenta);
+			Debug.DrawLine(perimetro[2].position, perimetro[3].position + new Vector3(0, 1, 0), Color.magenta);
+			Debug.DrawLine(perimetro[3].position, perimetro[0].position + new Vector3(0, 1, 0), Color.magenta);
+
+			// Linea #5
+			Debug.DrawLine(perimetro[0].position + new Vector3(0, 1, 0), perimetro[1].position + new Vector3(0, 1, 0), Color.red);
+			Debug.DrawLine(perimetro[1].position + new Vector3(0, 1, 0), perimetro[2].position + new Vector3(0, 1, 0), Color.red);
+			Debug.DrawLine(perimetro[2].position + new Vector3(0, 1, 0), perimetro[3].position + new Vector3(0, 1, 0), Color.red);
+			Debug.DrawLine(perimetro[3].position + new Vector3(0, 1, 0), perimetro[0].position + new Vector3(0, 1, 0), Color.red);
 		}
 	}
 }
