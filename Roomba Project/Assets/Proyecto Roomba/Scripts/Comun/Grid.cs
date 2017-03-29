40c9fea4f0fedc73f6fee024d18a12a6a16b0ac5
Moon Antonio <pinchodev91@gmail.com>
2017-03-29 02:03:46 +0200

    Generador de grid

---
+++ Roomba Project/Assets/Proyecto Roomba/Scripts/Comun/Grid.cs
@@ -0,0 +1,98 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Gid.cs (29/03/2017)															\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Grid de la zona												\\
+// Fecha Mod:		29/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Grid de la zona</para>
+	/// </summary>
+	public class Grid : MonoBehaviour 
+	{
+		#region Variables Publicas
+		/// <summary>
+		/// <para>Largo del grid</para>
+		/// </summary>
+		public int xGrid;										// Largo del grid
+		/// <summary>
+		/// <para>Ancho del grid</para>
+		/// </summary>
+		public int zGrid;										// Ancho del grid
+		/// <summary>
+		/// <para>Tiempo hasta el nuevo spawn</para>
+		/// </summary>
+		public float tiempoSpawn = 0.0f;						// Tiempo hasta el nuevo spawn
+		/// <summary>
+		/// <para>Todos los vertices del grid</para>
+		/// </summary>
+		public Vector3[] vertices;                              // Todos los vertices del grid
+		/// <summary>
+		/// <para>Si esta completada la generacion</para>
+		/// </summary>
+		public bool isCompletado = false;						// Si esta completada la generacion
+		#endregion
+
+		#region Init
+		/// <summary>
+		/// <para>Init Grid</para>
+		/// </summary>
+		private void Awake()// Init Grid
+		{
+			StartCoroutine(Generar());
+		}
+		#endregion
+
+		#region Metodos
+		/// <summary>
+		/// <para>Generamos el grid.</para>
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerator Generar()// Generamos el grid
+		{
+			// Esperamos hasta que termine el tiempo de spawn
+			WaitForSeconds wait = new WaitForSeconds(tiempoSpawn);
+			vertices = new Vector3[(xGrid + 1) * (zGrid + 1)];
+
+			// Generamos el array (Area)
+			for (int n = 0, z = 0; z <= zGrid; z++)
+			{
+				for (int x = 0; x <= xGrid; x++, n++)
+				{
+					vertices[n] = new Vector3(x, 0.5f, z);
+					yield return wait;
+				}
+			}
+
+			// Generacion completada
+			isCompletado = true;
+		}
+
+		/// <summary>
+		/// <para>Cuando renderiza</para>
+		/// </summary>
+		private void OnDrawGizmos()// Cuando renderiza
+		{
+			// Si no hay vertices return
+			if (vertices == null) return;
+
+			Gizmos.color = Color.black;
+
+			// Renderizamos los vertices
+			for (int n = 0; n < vertices.Length; n++)
+			{
+				Gizmos.DrawSphere(vertices[n], 0.1f);
+			}
+		}
+		#endregion
+	}
+}
--- Roomba Project/Assets/Proyecto Roomba/Scripts/Sistemas/Generador.cs
+++ Roomba Project/Assets/Proyecto Roomba/Scripts/Sistemas/Generador.cs
@@ -2,9 +2,9 @@
 //																				\\
 // Generador.cs (20/03/2017)													\\
 // Autor: Antonio Mateo (Moon Antonio) 									        \\
-// Descripcion:		Generador de obstaculos										\\
-// Fecha Mod:		20/03/2017													\\
-// Ultima Mod:		Version Inicial												\\
+// Descripcion:		Generador de obstaculos/Objetos								\\
+// Fecha Mod:		29/03/2017													\\
+// Ultima Mod:		Agregada funcionalidad										\\
 //******************************************************************************\\
 
 #region Librerias
@@ -15,61 +15,16 @@ using System.Collections.Generic;
 namespace MoonAntonio.Roomba
 {
 	/// <summary>
-	/// <para>Generador de obstaculos</para>
+	/// <para>Generador de obstaculos/Objetos</para>
 	/// </summary>
 	public class Generador : MonoBehaviour 
 	{
+		#region Variables Publicas
 
-		public Transform[] perimetro;
-			
+		#endregion
 
-		private List<GameObject> objetosSpawn = new List<GameObject>();
-
-
-
-		private void Start()
-		{
-			objetosSpawn.Add((Resources.Load("Objeto01")) as GameObject);
-			objetosSpawn.Add((Resources.Load("Objeto02")) as GameObject);
-			objetosSpawn.Add((Resources.Load("Objeto03")) as GameObject);
-		}
-
-		private void Update()
-		{
-			Ray();
-		}
-
-		private void Ray()
-		{
-			// Linea #1
-			Debug.DrawLine(perimetro[0].position, perimetro[1].position, Color.red);
-			Debug.DrawLine(perimetro[1].position, perimetro[2].position, Color.red);
-			Debug.DrawLine(perimetro[2].position, perimetro[3].position, Color.red);
-			Debug.DrawLine(perimetro[3].position, perimetro[0].position, Color.red);
-
-			// Linea #2
-			Debug.DrawLine(perimetro[0].position, perimetro[0].up * 10, Color.red);
-			Debug.DrawLine(perimetro[1].position, perimetro[1].up * 10, Color.red);
-			Debug.DrawLine(perimetro[2].position, perimetro[2].up * 10, Color.red);
-			Debug.DrawLine(perimetro[3].position, perimetro[3].up * 10, Color.red);
-
-			// Linea #3
-			Debug.DrawLine(perimetro[0].position + new Vector3(0, 1, 0), perimetro[1].position, Color.magenta);
-			Debug.DrawLine(perimetro[1].position + new Vector3(0, 1, 0), perimetro[2].position, Color.magenta);
-			Debug.DrawLine(perimetro[2].position + new Vector3(0, 1, 0), perimetro[3].position, Color.magenta);
-			Debug.DrawLine(perimetro[3].position + new Vector3(0, 1, 0), perimetro[0].position, Color.magenta);
-
-			// Linea #4
-			Debug.DrawLine(perimetro[0].position, perimetro[1].position + new Vector3(0, 1, 0), Color.magenta);
-			Debug.DrawLine(perimetro[1].position, perimetro[2].position + new Vector3(0, 1, 0), Color.magenta);
-			Debug.DrawLine(perimetro[2].position, perimetro[3].position + new Vector3(0, 1, 0), Color.magenta);
-			Debug.DrawLine(perimetro[3].position, perimetro[0].position + new Vector3(0, 1, 0), Color.magenta);
-
-			// Linea #5
-			Debug.DrawLine(perimetro[0].position + new Vector3(0, 1, 0), perimetro[1].position + new Vector3(0, 1, 0), Color.red);
-			Debug.DrawLine(perimetro[1].position + new Vector3(0, 1, 0), perimetro[2].position + new Vector3(0, 1, 0), Color.red);
-			Debug.DrawLine(perimetro[2].position + new Vector3(0, 1, 0), perimetro[3].position + new Vector3(0, 1, 0), Color.red);
-			Debug.DrawLine(perimetro[3].position + new Vector3(0, 1, 0), perimetro[0].position + new Vector3(0, 1, 0), Color.red);
-		}
+		#region Variables Privadas
+		private Grid grid;
+		#endregion
 	}
 }
