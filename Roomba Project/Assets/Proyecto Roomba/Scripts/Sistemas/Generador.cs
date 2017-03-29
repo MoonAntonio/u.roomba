7b7a630f66248f8ad6ecceb7082be27b6e9b01a6
Moon Antonio <pinchodev91@gmail.com>
2017-03-29 02:32:45 +0200

    Clear Code

--- Roomba Project/Assets/Proyecto Roomba/Scripts/Sistemas/Generador.cs
+++ Roomba Project/Assets/Proyecto Roomba/Scripts/Sistemas/Generador.cs
@@ -20,21 +20,40 @@ namespace MoonAntonio.Roomba
 	public class Generador : MonoBehaviour 
 	{
 		#region Variables Publicas
-		public float tiempoSpawn = 0f;
-		public int maxSpawn = 0;
-		public List<GameObject> objetos = new List<GameObject>();
+		/// <summary>
+		/// <para>Tiempo entre cada spawn</para>
+		/// </summary>
+		public float tiempoSpawn = 0f;										// Tiempo entre cada spawn
+		/// <summary>
+		/// <para>Maximo de objetos</para>
+		/// </summary>
+		public int maxSpawn = 0;											// Maximo de objetos
+		/// <summary>
+		/// <para>Objetos en la escena</para>
+		/// </summary>
+		public List<GameObject> objetos = new List<GameObject>();			// Objetos en la escena
 		#endregion
 
 		#region Variables Privadas
-		private Grid grid;
-
-		private List<GameObject> objetosPool = new List<GameObject>();
-
-		private float tiempoActual = 0f;
+		/// <summary>
+		/// <para>Grid del manager</para>
+		/// </summary>
+		private Grid grid;													// Grid del manager
+		/// <summary>
+		/// <para>Piscina de objetos que se pueden instanciar</para>
+		/// </summary>
+		private List<GameObject> objetosPool = new List<GameObject>();		// Piscina de objetos que se pueden instanciar
+		/// <summary>
+		/// <para>Tiempo de spawn actual</para>
+		/// </summary>
+		private float tiempoActual = 0f;									// Tiempo de spawn actual
 		#endregion
 
 		#region Init
-		private void Start()
+		/// <summary>
+		/// <para>Init de Generador</para>
+		/// </summary>
+		private void Start()// Init de Generador
 		{
 			// Asignamos los ajustes
 			grid = this.gameObject.GetComponentInChildren(typeof(Grid)) as Grid;
@@ -46,19 +65,29 @@ namespace MoonAntonio.Roomba
 		#endregion
 
 		#region Actualizador
-		private void Update()
+		/// <summary>
+		/// <para>Actualizador de Generador</para>
+		/// </summary>
+		private void Update()// Actualizador de Generador
 		{
+			// Comprobamos si emos excedido el max de objetos o si el grid esta cargando
 			if (objetos.Count >= maxSpawn || grid.isCompletado == false) return;
 
+			// Aumentamos el contador
 			Contador();
 		}
 		#endregion
 
 		#region Metodos
-		private void Contador()
+		/// <summary>
+		/// <para>Actualiza el tiempo.Contador que ejecuta <see cref="Generar(int)"/> al cabo de un tiempo en <see cref="tiempoSpawn"/>.</para>
+		/// </summary>
+		private void Contador()// Actualiza el tiempo.Contador que ejecuta <see cref="Generar(int)"/> al cabo de un tiempo en <see cref="tiempoSpawn"/>
 		{
+			// Contador
 			tiempoActual = tiempoActual + 1 * Time.deltaTime;
 
+			// Reseteo y generacion de objeto
 			if (tiempoActual >= tiempoSpawn)
 			{
 				Generar(Random.Range(0,2));
@@ -66,7 +95,11 @@ namespace MoonAntonio.Roomba
 			}
 		}
 
-		private void Generar(int n)
+		/// <summary>
+		/// <para>Genera un Objeto en la zona mediante una posicion dada en <see cref="Grid.GetPosicionAleatoria"/>.</para>
+		/// </summary>
+		/// <param name="n">Tipo de Objeto que instanciar de <see cref="objetosPool"/>.</param>
+		private void Generar(int n)// Genera un Objeto en la zona mediante una posicion dada en <see cref="Grid.GetPosicionAleatoria"/>
 		{
 			switch (n)
 			{
